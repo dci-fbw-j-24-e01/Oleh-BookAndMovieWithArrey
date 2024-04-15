75c3bda91507f51169d154ce9cfa5ddafb09bbd6
Herr Oleh Bryl <olegbryl@gmail.com>
2024-04-15 14:16:13 +0200

    finally

--- src/AddMovie.java
+++ src/AddMovie.java
@@ -36,47 +36,48 @@ public class AddMovie {
     public static void printMovieDetails(HashMap<Integer, Movie> movies) {
         int choice = scanner.nextInt();
         if (choice == 1) {
-            System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d", movies.get(1).getTitle(), movies.get(1).getGenre(), movies.get(1).getPrice(), movies.get(1).getProductId());
+            System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d\n", movies.get(1).getTitle(), movies.get(1).getGenre(), movies.get(1).getPrice(), movies.get(1).getProductId());
         }
         if (choice == 2) {
-            System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d", movies.get(2).getTitle(), movies.get(2).getGenre(), movies.get(2).getPrice(), movies.get(2).getProductId());
+            System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d\n", movies.get(2).getTitle(), movies.get(2).getGenre(), movies.get(2).getPrice(), movies.get(2).getProductId());
         }
         if (choice == 3) {
-            System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d", movies.get(3).getTitle(), movies.get(3).getGenre(), movies.get(3).getPrice(), movies.get(3).getProductId());
+            System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d\n", movies.get(3).getTitle(), movies.get(3).getGenre(), movies.get(3).getPrice(), movies.get(3).getProductId());
         }
         if (choice == 4) {
-            System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d", movies.get(4).getTitle(), movies.get(4).getGenre(), movies.get(4).getPrice(), movies.get(4).getProductId());
+            System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d\n", movies.get(4).getTitle(), movies.get(4).getGenre(), movies.get(4).getPrice(), movies.get(4).getProductId());
         }
         if (choice == 5) {
-            System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d", movies.get(5).getTitle(), movies.get(5).getGenre(), movies.get(5).getPrice(), movies.get(5).getProductId());
+            System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d\n", movies.get(5).getTitle(), movies.get(5).getGenre(), movies.get(5).getPrice(), movies.get(5).getProductId());
         }
 
     }
 
     private void printMoviesDetails2(HashMap<Integer, Movie> movies) {
-        System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d", movies.get(1).getTitle(), movies.get(1).getGenre(), movies.get(1).getPrice(), movies.get(1).getProductId());
+        System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d\n", movies.get(1).getTitle(), movies.get(1).getGenre(), movies.get(1).getPrice(), movies.get(1).getProductId());
         System.out.println("---");
-        System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d", movies.get(2).getTitle(), movies.get(2).getGenre(), movies.get(2).getPrice(), movies.get(2).getProductId());
+        System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d\n", movies.get(2).getTitle(), movies.get(2).getGenre(), movies.get(2).getPrice(), movies.get(2).getProductId());
         System.out.println("---");
-        System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d", movies.get(3).getTitle(), movies.get(3).getGenre(), movies.get(3).getPrice(), movies.get(3).getProductId());
+        System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d\n", movies.get(3).getTitle(), movies.get(3).getGenre(), movies.get(3).getPrice(), movies.get(3).getProductId());
         System.out.println("---");
-        System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d", movies.get(4).getTitle(), movies.get(4).getGenre(), movies.get(4).getPrice(), movies.get(4).getProductId());
+        System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d\n", movies.get(4).getTitle(), movies.get(4).getGenre(), movies.get(4).getPrice(), movies.get(4).getProductId());
         System.out.println("---");
-        System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d", movies.get(5).getTitle(), movies.get(5).getGenre(), movies.get(5).getPrice(), movies.get(5).getProductId());
+        System.out.printf("Title: %s\nAuthor: %s\nPrice: %d $ \nProductId: %d\n", movies.get(5).getTitle(), movies.get(5).getGenre(), movies.get(5).getPrice(), movies.get(5).getProductId());
     }
 
     public static void findMovieById(HashMap<Integer, Movie> movies) {
 
         long c = scanner.nextLong();
+        long foundKey = 0;
         for (int key : movies.keySet()) {
             if (c == movies.get(key).getProductId()) {
-
+                foundKey = key;
                 System.out.printf("Titel: %s\nAuthor: %s\nPrice: %d $", movies.get(key).getTitle(), movies.get(key).getGenre(), movies.get(key).getPrice());
             }
-//            else {
-//                System.out.println("Seriously? And where should I look for a movie with that ID? " + c + " It's not here!");
-//                break;
-//            }
+
+        }
+        if (foundKey == 0){
+            System.out.println("Seriously? And where should I look for a movie with that ID? " + c + " - It's not here!");
         }
     }
 }
